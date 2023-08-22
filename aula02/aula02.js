42ee7de2b4df7a515809c821a443dd4d7d20a95b
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2023-08-22 10:34:44 -0300

    aula02

---
+++ aula02/aula02.js
@@ -0,0 +1,6 @@
+"use strict" //Modo Estrito
+
+let nome="Thomaz" //No modo strict precisa declarar a variavel para funcionar
+console.log(nome)
+console.log("JavaScript")
+console.log("Nome: " + nome)
