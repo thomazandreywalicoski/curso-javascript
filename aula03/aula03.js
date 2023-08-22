2cd19d32fde0eafbe26d088d789be86348fb4c76
Thomaz Andrey Walicoski <138054605+thomazandreywalicoski@users.noreply.github.com>
2023-08-22 11:08:05 -0300

    aula03

---
+++ aula03/aula03.js
@@ -0,0 +1,18 @@
+"use strict"
+
+function teste(){
+    let nome="Thomaz"
+    if(true){
+       console.log("dentro do if do teste: " + nome) 
+    }
+    console.log("dentro de teste: " + nome)
+}
+
+teste()
+
+console.log("fora de teste: " + nome)
+
+
+const curso="JavaScript" //Não posso mudar o valor de uma constante
+
+console.log(curso)
